818ebb9ed54fb0c377aaa4c5696424bbf95e4345
klarynny <49276346+klarynny@users.noreply.github.com>
2019-09-12 10:02:24 -0300

    herança
    códigos com aplicação de herança

---
+++ Aluno.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.iffiana.heranca;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aluno extends Pessoa {
+
+    public Aluno(String _nome, String _cpf) {
+        super(_nome, _cpf);
+    }
+    public String matricula;
+}
---
+++ Pessoa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.iffiana.heranca;
+
+/**
+ *
+ * @author aluno
+ */
+public class Pessoa {
+
+         public String nome;
+         public String cpf;
+     
+    }
+    

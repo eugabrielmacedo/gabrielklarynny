52120a688a6b010952996f93f3b593a3cb92cbb3
eugabrielmacedo <gabrielmacedo.ps@gmail.com>
2019-11-07 10:40:53 -0200

    novos pacotes

--- Formulariokg/src/main/java/br/com/kg/formulario/entidades/Usuario.java
+++ Formulariokg/src/main/java/br/com/kg/formulario/entidades/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.kg.formulariokg;
+package br.com.kg.formulario.entidades;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
